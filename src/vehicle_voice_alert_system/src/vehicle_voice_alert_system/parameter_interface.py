93cc1bc65594d2f0019fd6b16bf233f2a9845f85
tkhmy <tkh.my.p@gmail.com>
2023-03-22 11:21:25 +0900

    fix node
    Signed-off-by: tkhmy <tkh.my.p@gmail.com>

--- src/vehicle_voice_alert_system/src/vehicle_voice_alert_system/parameter_interface.py
+++ src/vehicle_voice_alert_system/src/vehicle_voice_alert_system/parameter_interface.py
@@ -29,37 +29,35 @@ class ParameterInterface:
         self.parameter = GeneralParameter()
         self.mute_parameter = MuteParameter()
 
-        self._node.declare_parameter("manual_driving_bgm", False)
-        self._node.declare_parameter("skip_default_voice", False)
-        self._node.declare_parameter("mute_overlap_bgm", False)
-        self._node.declare_parameter("driving_velocity_threshold", 0.2)
-        self._node.declare_parameter("primary_voice_folder_path", "")
+        node.declare_parameter("manual_driving_bgm", False)
+        node.declare_parameter("skip_default_voice", False)
+        node.declare_parameter("mute_overlap_bgm", False)
+        node.declare_parameter("driving_velocity_threshold", 0.2)
+        node.declare_parameter("primary_voice_folder_path", "")
 
         self.parameter.manual_driving_bgm = (
-            self._node.get_parameter("manual_driving_bgm").get_parameter_value().bool_value
+            node.get_parameter("manual_driving_bgm").get_parameter_value().bool_value
         )
         self.parameter.skip_default_voice = (
-            self._node.get_parameter("skip_default_voice").get_parameter_value().bool_value
+            node.get_parameter("skip_default_voice").get_parameter_value().bool_value
         )
         self.parameter.mute_overlap_bgm = (
-            self._node.get_parameter("mute_overlap_bgm").get_parameter_value().bool_value
+            node.get_parameter("mute_overlap_bgm").get_parameter_value().bool_value
         )
         self.parameter.driving_velocity_threshold = (
-            self._node.get_parameter("driving_velocity_threshold")
-            .get_parameter_value()
-            .double_value
+            node.get_parameter("driving_velocity_threshold").get_parameter_value().double_value
         )
         self.parameter.primary_voice_folder_path = (
-            self._node.get_parameter("primary_voice_folder_path").get_parameter_value().string_value
+            node.get_parameter("primary_voice_folder_path").get_parameter_value().string_value
         )
 
-        self._node.declare_parameter("mute_timeout.restart_engage", 0.0)
-        self._node.declare_parameter("mute_timeout.accept_start", 0.0)
-        self._node.declare_parameter("mute_timeout.stop_reason", 0.0)
-        self._node.declare_parameter("mute_timeout.turn_signal", 0.0)
-        self._node.declare_parameter("mute_timeout.in_emergency", 0.0)
-        self._node.declare_parameter("mute_timeout.driving_bgm", 0.0)
-        mute_timeout_prefix = self._node.get_parameters_by_prefix("mute_timeout")
+        node.declare_parameter("mute_timeout.restart_engage", 0.0)
+        node.declare_parameter("mute_timeout.accept_start", 0.0)
+        node.declare_parameter("mute_timeout.stop_reason", 0.0)
+        node.declare_parameter("mute_timeout.turn_signal", 0.0)
+        node.declare_parameter("mute_timeout.in_emergency", 0.0)
+        node.declare_parameter("mute_timeout.driving_bgm", 0.0)
+        mute_timeout_prefix = node.get_parameters_by_prefix("mute_timeout")
 
         for key in mute_timeout_prefix.keys():
             setattr(
